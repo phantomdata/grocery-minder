b65d856887d4d36df48212d97a9884358f16f8f8
phantomdata <noreply@phantomdata.com>
2016-02-16 21:55:52 -0600

    Adding basic controller for GroceryItems to validate autofac setup

---
+++ PhantomGroceries.Web/Controllers/GroceryItemsController.cs
@@ -0,0 +1,27 @@
+﻿using PhantomGroceries.Service.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhantomGroceries.Web.Controllers
+{
+    [Authorize]
+    public class GroceryItemsController : Controller
+    {
+        private readonly IGroceryItemService groceryItemService;
+
+        public GroceryItemsController(IGroceryItemService groceryItemService)
+        {
+            this.groceryItemService = groceryItemService;
+        }
+
+        // GET: GroceryItems
+        public ActionResult Index()
+        {
+            var groceryItems = groceryItemService.GetAll("test");
+            return View();
+        }
+    }
+}
