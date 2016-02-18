746dcd27fd3906340adbd3e83cd77ca19a3f6bc3
phantomdata <noreply@phantomdata.com>
2016-02-17 20:57:17 -0600

    Adding GroceryCategories to help organize groceries

---
+++ GroceryMinder.Domain/Models/GroceryCategory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryMinder.Domain.Models
+{
+    public class GroceryCategory
+    {
+        [Key]
+        public int GroceryCategoryId { get; set; }
+
+        [ForeignKey("ApplicationUserId")]
+        public virtual ApplicationUser ApplicationUser { get; set; }
+
+        [Index]
+        public string ApplicationUserId { get; set; }
+
+        [Display(Name = "Name")]
+        [MaxLength(255)]
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ GroceryMinder.Service/Services/GroceryCategoryService.cs
@@ -0,0 +1,61 @@
+﻿using GroceryMinder.Data.Repositories;
+using GroceryMinder.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryMinder.Service.Services
+{
+    public interface IGroceryCategoryService
+    {
+        void Create(GroceryCategory groceryItem);
+        void Delete(GroceryCategory item);
+        GroceryCategory Get(string userId, int groceryItemId);
+        IEnumerable<GroceryCategory> GetAll(string userId);
+        void Update(GroceryCategory groceryItem);
+    }
+    public class GroceryCategoryService : IGroceryCategoryService
+    {
+        private readonly IGroceryCategoryRepository groceryCategoryRepository;
+
+        public GroceryCategoryService(IGroceryCategoryService groceryItemRepository)
+        {
+            this.groceryCategoryRepository = groceryCategoryRepository;
+        }
+
+        public void Create(GroceryCategory item)
+        {
+            groceryCategoryRepository.Add(item);
+            groceryCategoryRepository.Commit();
+            return;
+        }
+
+        public void Delete(GroceryCategory item)
+        {
+            groceryCategoryRepository.Delete(item);
+            groceryCategoryRepository.Commit();
+        }
+
+        public GroceryCategory Get(string userId, int itemId)
+        {
+            var ret = groceryCategoryRepository.Get(x => x.ApplicationUserId == userId && x.GroceryCategoryId == itemId);
+            return ret;
+        }
+
+        public IEnumerable<GroceryCategory> GetAll(string userId)
+        {
+            var ret = groceryCategoryRepository.GetAll().Where(x => x.ApplicationUserId == userId);
+
+            return ret;
+        }
+
+        public void Update(GroceryCategory item)
+        {
+            groceryCategoryRepository.Update(item);
+            groceryCategoryRepository.Commit();
+            return;
+        }
+    }
+}
