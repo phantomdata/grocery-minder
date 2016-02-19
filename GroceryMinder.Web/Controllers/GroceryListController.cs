6e6184e42ef263031018aabcac7205b249f3a3c5
phantomdata <noreply@phantomdata.com>
2016-02-19 00:14:05 -0600

    Adding a basic grocery list

---
+++ GroceryMinder.Web/Controllers/GroceryListController.cs
@@ -0,0 +1,49 @@
+﻿using GroceryMinder.Service.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GroceryMinder.Web.Controllers
+{
+    public class GroceryListController : Controller
+    {
+        private string _userId { get; set; } // Not meant to be used beyond following scope
+        private string UserId
+        {
+            get
+            {
+                if (_userId == null)
+                {
+                    _userId = User.Identity.GetUserId();
+                }
+                return _userId;
+            }
+        }
+
+        private readonly IGroceryService groceryService;
+
+        public GroceryListController(IGroceryService groceryService)
+        {
+            this.groceryService = groceryService;
+        }
+
+        public ActionResult Index()
+        {
+            // Give some fudge in case they decide to go shopping early
+            var cutoffDate = DateTime.Today.AddDays(-3);
+
+            var vm = new Models.GroceryList.Index();
+
+            vm.GroceryList = groceryService.GetAll(UserId)
+                .Where(g => g.NextPurchaseAt <= cutoffDate)
+                .OrderBy(g => g.GroceryCategory.Name)
+                .ThenBy(g => g.Name)
+                .ToList();
+
+            return View(vm);
+        }
+    }
+}
---
+++ GroceryMinder.Web/Models/GroceryList/Index.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroceryMinder.Web.Models.GroceryList
+{
+    public class Index
+    {
+        public IList<Domain.Models.Grocery> GroceryList { get; set; }
+    }
+}
